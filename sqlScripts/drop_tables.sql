896c213eaafb4ca2e5a7a53ea917548e0c8c9c27
Matthew Gordon <mgordon@umail.ucsb.edu>
2016-11-27 23:52:44 -0800

    added drop tables script that drops all table so we can reset the schema.

---
+++ sqlScripts/drop_tables.sql
@@ -0,0 +1,23 @@
+DROP TABLE Manager;
+
+DROP TABLE Is_friends;
+
+DROP TABLE Topic_Message;
+
+DROP TABLE Topic_User;
+
+DROP TABLE Topic;
+
+DROP TABLE Circle;
+
+DROP TABLE Private_Messages;
+
+DROP TABLE Member_Of;
+
+DROP TABLE Group_Messages;
+
+DROP TABLE Group_Owner;
+
+DROP TABLE Messages;
+
+DROP TABLE Users;
